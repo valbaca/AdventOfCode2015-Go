a6d4e521e8a71285f41b34240239f587e43bca31
Val Baca <valbaca@gmail.com>
2019-03-26 07:29:25 +0000

    Refactor main.go
    New usage:
    $ ./advent2015 [day]

--- main.go
+++ main.go
@@ -1,22 +1,43 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strconv"
 
 	"valbaca.com/advent2015/day1"
 )
 
+const MAX_DAY = 1
+
 func main() {
-	fmt.Println(day1.Part2(readFromStdin()))
+	day := getDay()
+	input := readInputFile(day)
+	fmt.Println(day1.Part2(input))
 }
 
-func readFromStdin() string {
-	reader := bufio.NewReader(os.Stdin)
-	in, err := reader.ReadString('\n')
+func getDay() int {
+	if len(os.Args) < 2 {
+		usageExit()
+	}
+	day, err := strconv.Atoi(os.Args[1])
+	if err != nil || day < 1 || day > MAX_DAY {
+		usageExit()
+	}
+	return day
+}
+
+func usageExit() {
+	fmt.Fprintln(os.Stderr, "Usage: [day]\nday must be int")
+	os.Exit(1)
+}
+
+func readInputFile(day int) string {
+	name := fmt.Sprintf("./inputs/day%v.txt", day)
+	out, err := ioutil.ReadFile(name)
 	if err != nil {
 		panic(err)
 	}
-	return in
+	return string(out)
 }
