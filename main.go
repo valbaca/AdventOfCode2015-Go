739b1beed8a02e714f526fb4c918592c1b1ebf91
Val Baca <valbaca@gmail.com>
2019-03-28 04:06:20 +0000

    minor redo main

--- main.go
+++ main.go
@@ -11,22 +11,17 @@ import (
 	"valbaca.com/advent2015/day3"
 )
 
-const MAX_DAY = 1
-
 func main() {
 	day := getDay()
 	// TODO don't read whole file in memory
 	input := readInputFile(day)
 	switch day {
 	case 1:
-		fmt.Println(day1.Part1(input))
-		fmt.Println(day1.Part2(input))
+		fmt.Println(day1.Part1(input), day1.Part2(input))
 	case 2:
-		fmt.Println(day2.Part1(input))
-		fmt.Println(day2.Part2(input))
+		fmt.Println(day2.Part1(input), day2.Part2(input))
 	case 3:
-		fmt.Println(day3.Part1(input))
-		fmt.Println(day3.Part2(input))
+		fmt.Println(day3.Part1(input), day3.Part2(input))
 	}
 }
 
